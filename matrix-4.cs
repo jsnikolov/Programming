833335379f6ff217ac915f46f6a02c2d29bc5dde
jsnikolov <jsnikolov@gmail.com>
2014-01-18 20:33:54 +0200

    Update matrix-4.cs

--- matrix-4.cs
+++ matrix-4.cs
@@ -1,11 +1,13 @@
-if(((rows&1) === 0) && ((i&1) === 0) && ((innerLength&1) === 0)){
-		row++;    
+if(((rows&1) === 0) && ((i&1) === 0) && ((innerLength&1) === 0))
+{
+	row++;    
 } 
 else if(((rows&1) !== 0) && ((i&1) === 0) && ((innerLength&1) !== 0))
 {
 	row++;
 }
-if(((rows&1) === 0) && ((i&1) == 0) && ((innerLength&1) != 0)){
+if(((rows&1) === 0) && ((i&1) == 0) && ((innerLength&1) != 0))
+{
 	row--;    
 } 
 else if(((rows&1) !== 0) && ((i&1) === 0) && ((innerLength&1) === 0))
@@ -19,10 +21,11 @@ else if(((rows&1) !== 0) && ((i&1) !== 0) && ((innerLength&1) !== 0))
 {
 	col--;
 }              
-if(((rows&1) === 0) && ((i&1) != 0) && ((innerLength&1) != 0)){
+if(((rows&1) === 0) && ((i&1) != 0) && ((innerLength&1) != 0))
+{
 	col++;
 } 
 else if(((rows&1) !== 0) && ((i&1) !== 0) && ((innerLength&1) === 0))
 {
 	col++;
-}  
+}
